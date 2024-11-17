22355339b0562e203d5ad41259207413c2ea18ab
Yincard <isbestsalt@gmail.com>
2024-11-16 22:42:16 -0500

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <p className="text-center text-xs text-muted-foreground">
           <span>&copy; {new Date().getFullYear()}</span>{" "}
           <Link className="link" href="/">
-            localhost:3000
+            malam.me
           </Link>
         </p>
       </section>
